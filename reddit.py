dffaa375157fd7b866c5df5573cf7ce36ed8bd57
Tom Bowden <tomfbowden12@gmail.com>
2019-04-08 04:36:11 +1000

    bot now waits 30 seconds after disconnecting and tries again

--- reddit.py
+++ reddit.py
@@ -46,39 +46,45 @@ def main(debug=False):
     subreddit = reddit.subreddit(subredditToStream)
     logging.basicConfig(filename='maro-logs/maro.log', level=logging.INFO)
 
-    print("beginning to stream /r/{}".format(subreddit.display_name))
-    for post in subreddit.stream.submissions():
-        url = post.url
-        print("post found - {} {}".format(post.title, url))
-        # check its from markrosewater.tumblr.com
-        if keyword in url:
-            # hide the post done so we won't see it again
-            # we're happy to keep seeing repeats of old posts though
-            post.hide()
-            # get id
-            paths = url.split('/')
-            if len(paths) < 3:
-                # no id
-                logging.warn("could not identify id from url {} - not enough segments".format(url))
-            else:
-                id = None
-                for path in paths:
-                    if path.isdigit():
-                        id = path
-                if id is None:
-                    logging.warn("Could not identify id from url {} - no matching integer segments".format(url))
-                else:
-                    try:
-                        print("Creating post with tumblr id={}".format(id))
-                        if debug == False:
-                            create_reply(post, id)
+    while True:
+        print("beginning to stream /r/{}".format(subreddit.display_name))
+        try:
+            for post in subreddit.stream.submissions():
+                url = post.url
+                print("post found - {} {}".format(post.title, url))
+                # check its from markrosewater.tumblr.com
+                if keyword in url:
+                    # hide the post done so we won't see it again
+                    # we're happy to keep seeing repeats of old posts though
+                    post.hide()
+                    # get id
+                    paths = url.split('/')
+                    if len(paths) < 3:
+                        # no id
+                        logging.warn("could not identify id from url {} - not enough segments".format(url))
+                    else:
+                        id = None
+                        for path in paths:
+                            if path.isdigit():
+                                id = path
+                        if id is None:
+                            logging.warn("Could not identify id from url {} - no matching integer segments".format(url))
                         else:
-                            print("creating fake response to tumblr {}, reddit {}".format(id, post.url))
-                    except TumblrPostNotFoundException as e:
-                        logging.warn("Tried looking but could not find post with id {}".format(id))
-                    except TumblrNotAQuestionPostException as e:
-                        logging.warn("found post with id {} but was not in the form of a question".format(id))
-
+                            try:
+                                print("Creating post with tumblr id={}".format(id))
+                                if debug == False:
+                                    create_reply(post, id)
+                                else:
+                                    print("creating fake response to tumblr {}, reddit {}".format(id, post.url))
+                            except TumblrPostNotFoundException as e:
+                                logging.warn("Tried looking but could not find post with id {}".format(id))
+                            except TumblrNotAQuestionPostException as e:
+                                logging.warn("found post with id {} but was not in the form of a question".format(id))
+        except Exception as e:
+            logging.error("Submission streaming broke with error: {}".format(e))
+            print("Streaming broke, sleeping then trying again (check logs for error)")
+            time.sleep(30)
+            logging.info("Restarting streaming...")
 
 if __name__ == "__main__":
     if len(argv) > 1:
